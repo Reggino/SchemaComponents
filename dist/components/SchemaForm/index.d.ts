5ce515d35d6766ab25be574fb7cb716de24e0a08
Ariyadhana Sunardi <ari@mediainfogrup.com>
2023-05-19 16:15:48 +0700

    update dist folder

--- dist/components/SchemaForm/index.d.ts
+++ dist/components/SchemaForm/index.d.ts
@@ -1,21 +1,8 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import { oas31 } from "openapi3-ts";
+import { IFieldConfig } from "../../type";
 import "react-datepicker/dist/react-datepicker.css";
 import "./index.scss";
-export type TInputType = "string" | "integer" | "date" | "date-time" | "boolean" | "array" | "object";
-export interface IFieldConfig {
-    extractValue?: (key: string, val: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>, index?: number, type?: TInputType) => any;
-    schema?: oas31.SchemaObject;
-    disabled?: boolean;
-    hidden?: boolean;
-    component?: string;
-    style?: React.CSSProperties;
-    inputType?: "select" | "input" | "date";
-    options?: {
-        [key: string]: string;
-    }[];
-    isUrl?: boolean;
-}
 type ISchemaFormProps<T> = {
     schema: oas31.SchemaObject;
     value: T;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,4 @@
 import SchemaForm from "./components/SchemaForm";
+import { IFieldConfig, TInputType } from "./type";
+export type { IFieldConfig, TInputType };
 export { SchemaForm };
